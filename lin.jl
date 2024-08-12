178c19d93be5d1c42ea9cee3e8e8aa2e5897946c
yuvraj-singh-math <fdmm79@durham.ac.uk>
2024-08-12 10:51:51 +0100

    Update lin.jl

--- lin.jl
+++ lin.jl
@@ -17,27 +17,39 @@ system=[x^2+y^2+2x+3y+7x^2*y^3,x^2+y^7+2x^2+3y^3+7x+2y+x^7*y^7,x^2+y^2+1]
 conv_hulls=[convex_hull(collect(Nemo.exponent_vectors(f))) for f in system]
 
 # return a list of vectors paired with a point
-function convex_hull_edge_vectors(conv)
-    # vertices.faces(conv,1) selects all edges, returned as the two points it lies between
-    # we calculate the difference
-    edges=[[m[1]-m[2],m[2]] for m in vertices.(faces(conv,1))]
-    return edges
+function convex_hull_edge_vectors(convs)
+    i=0
+    listofedges=[]
+    for conv in convs
+        i+=1
+        # structure of an edge element: vector describing the edge, point of origin, and the i-th system its associated to (needed for applying the monomial map later)
+        push!(listofedges,[[m[1]-m[2],m[2],i] for m in vertices.(faces(conv,1))])
+    end
+    return listofedges
 end
 
 function aligned_edges(listofedges)
     maxdim=length(listofedges)
     # we choose k elements from the list
     nchoosek=[collect(Combinatorics.combinations(listofedges,k)) for k in 2:maxdim]
-    possible_combinations=[collect(Iterators.product(c...)) for j in nchoosek for c in j]
-    println(possible_combinations)
-    # if we have perfect matches, greedily take those first
+    # above, nchoosek is in the form [ [subsets of 2], [subsets of 3], ... [subsets of n]]. concatenate these
+    nchoosek=vcat(nchoosek...)
+    possible_combinations=[collect(Iterators.product(j...)) for j in nchoosek]
+    # Iterators.product returns a matrix, convert this into a vector
     possible_combinations=[vec(j) for j in possible_combinations]
-    compared=[filter!(m->prod([c[1]==m[1][1]||c[1]==-m[1][1] for c in m]),j) for j in possible_combinations]
-    compared=filter!(m->m!=[],compared)
-    return compared
+    possible_combinations=vcat(possible_combinations...)
+    # if we have perfect matches, greedily take those first
+    perfect_alignment=filter!(m->prod([c[1]==m[1][1]||c[1]==-m[1][1] for c in m]),possible_combinations)
+    perfect_alignment=filter!(m->m!=[],perfect_alignment)
+    println(perfect_alignment)
+    # filter this down further by seeing which edge alignments can be attained by a monomial map
+    # map to gens(S) TODO
+    perfect_maps=[[(x[2]-alignment[1][2]) for x in alignment] for alignment in perfect_alignment]
+    return perfect_maps
     # otherwise, settle for parallel edges
     # TODO
 end
+
 
 function matrix_from_system(pol_system)
     mons=unique(collect(Iterators.flatten([collect(monomials(f)) for f in pol_system])))
