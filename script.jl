772c93c2715337ae310ba43c176d99fa390cb2c3
yuvraj-singh-math <fdmm79@durham.ac.uk>
2024-08-30 18:06:18 +0100

    Add types to code, make it significantly faster

--- script.jl
+++ script.jl
@@ -17,44 +17,63 @@ struct OdebaseNode
     param_polynomial_system::Vector
     # should replace this with a function soon
     generic_polynomial_system::Vector
-    constraints
+    constraints::Vector
     paramsRing
-    polRing
+    # for now, rational (remove when not in script)
+    polRing::QQMPolyRing
 end
 
 # The initial values for rejects are defined by those systems that have a parameter to the power of another parameter (eg k1^k2)
 # We do not save these as .jl file to begin with as of right now
 rejects=Dict(vcat([id=>"Fails to load in Julia" for id in ["BIOMD0000000060","BIOMD0000000637"]],["BIOMD0000000205"=>"Contains monomial equation"])...)
-odebaseSystems=[]
+odebaseSystems=OdebaseNode[]
+
+function rand_nonzero(len::Int)
+    ints=Int[]
+    for x in 1:len
+        num=rand(Int8)
+        while num==0
+            num=rand(Int8)
+        end
+        push!(ints,num)
+    end
+    return ints
+end
 
 for file in chems
     include(file);
-    randCoeff=rand(Int8,length(gens(paramsRing)));
-    phi=hom(polRing,polRing,c->evaluate(c,randCoeff),gens(polRing));
-    push!(odebaseSystems, OdebaseNode(name,true,true,1,1,length(gens(polRing)),chemSystem,[phi(x) for x in chemSystem],[],paramsRing,polRing));
+    randCoeff=rand_nonzero(length(gens(paramsRing)));
+    QQpolRing,tup=polynomial_ring(QQ,["$x" for x in gens(polRing)]);
+    phi=hom(polRing,QQpolRing,c->evaluate(c,randCoeff),gens(QQpolRing));
+    # we redefine polRing to be of rational type after the map
+    push!(odebaseSystems, OdebaseNode(name,true,true,1,1,length(gens(polRing)),chemSystem,[phi(x) for x in chemSystem],[],paramsRing,QQpolRing));
 end
 
-unfiltered_systems=[OdebaseNode(sys.ID,true,true,1,1,sys.numSpecies,sys.param_polynomial_system,filter(l->!iszero(l),unique(sys.generic_polynomial_system)),[],sys.paramsRing,sys.polRing) for sys in odebaseSystems]
+unfiltered_systems=[OdebaseNode(sys.ID,true,true,1,1,sys.numSpecies,sys.param_polynomial_system,filter(l->!iszero(l),unique(sys.generic_polynomial_system)),[],sys.paramsRing,sys.polRing) for sys in odebaseSystems];
 
 for sys in odebaseSystems
     # Note that for f=2*x1, even though this is monomial, and has no toric solutions, is_monomial returns false
  #    so we look at the length of the list of monomials, check if its 1
     if sum([length(collect(monomials(f)))==1 for f in sys.generic_polynomial_system])>0
-        rejects[sys.ID]="Contains monomial equation"
-        filter!(s->s!=sys,unfiltered_systems)
+        rejects[sys.ID]="Contains monomial equation";
+        filter!(s->s!=sys,unfiltered_systems);
     end
 
     if length(unique(collect(Iterators.flatten([collect(monomials(f)) for f in sys.generic_polynomial_system]))))<length(sys.generic_polynomial_system)
-        rejects[sys.ID]="Macaulay matrix has more rows than columns"
-        filter!(s->s!=sys,unfiltered_systems)
+        rejects[sys.ID]="Macaulay matrix has more rows than columns";
+        filter!(s->s!=sys,unfiltered_systems);
     end
 end
 
-# unfiltered_systems=filter(s->s.numSpecies<6,unfiltered_systems)
-const systems=copy(unfiltered_systems)
+# quick approximation for complexity. better would be to compute the upper bound on no. of fully supported minors
+## uncomment the two lines below to filter out any systems with number of species>=upperBound
+#upperbound=89
+#unfiltered_systems=filter(s->s.numSpecies<upperbound,unfiltered_systems);
+unfiltered_systems=sort(unfiltered_systems,by= x->x.numSpecies);
+const systems=copy(unfiltered_systems);
 
 # We return the 2n perturbations of degree 1 as well as the system itself
-function perturbSystem(system)
+function perturbSystem(system::OdebaseNode)
     trans=[]
     for m in gens(system.polRing)
         for k in 1:length(system.generic_polynomial_system)
@@ -73,16 +92,17 @@ function IDToODE(ID)
 end
 
 
+# We work globally with QQMatrix since our generic_polynomial_system has rational coefficients
 function matrix_from_system(pol_system)
     mons=unique(collect(Iterators.flatten([collect(monomials(f)) for f in pol_system])))
     S = matrix_space(QQ, length(pol_system), length(mons))
-    M_list=collect(Iterators.flatten(([[coeff(f,m) for m in mons] for f in pol_system])))
+    M_list=collect(Iterators.flatten(([[QQ(coeff(f,m)) for m in mons] for f in pol_system])))
     M=matrix(QQ,length(pol_system),length(mons),M_list)
     return M
 end
 
 
-function is_det_zero(mat)
+function is_det_zero(mat::QQMatrix)
     # returns "no method matching AbstractFloat"??
     if number_of_columns(mat)==number_of_rows(mat)
         return !(rank(mat)==number_of_rows(mat))
@@ -90,110 +110,60 @@ function is_det_zero(mat)
     error("not square")
 end
 
-# Given a list of n subsets, we want to choose one element from each such that we do not choose the same element twice
-# This recursive function takes a path from the first subset to the last, and then selects from there
-function niceprod(arrcols,illegal)
-    paths=[]
-    if length(arrcols)>1
-        for element in arrcols[1]
-            if !(element in illegal)
-            push!(paths,vcat.(Ref(element),niceprod(arrcols[2:end],vcat(element,illegal)))...)
-            end
-        end
-        return paths
-    else
-        # return all the elements that haven't been repeated earlier in the 
-        push!(paths,filter(m->!(m in illegal),arrcols[1])...)
-    end
-    return paths
-end
-
-function niceprod_nonrecur(arrcols)
+function niceprod_nonrecur(arrcols::Vector{Vector{Int}})
     # We start from the biggest set because this is most likely* to contain the most duplicates
     # * for ODEBASE systems with a lot of overlap
     # worst case is being of the same complexity as Iterators.product()
-    arrcols=sort(arrcols, rev=true)
-    paths=[[element] for element in arrcols[1]]
-    n=2
+    arrcols=sort(arrcols,by= x->length(x), rev=true)
+    paths=Set{Set{Int}}([Set{Int}([element]) for element in arrcols[1]])
     for set in arrcols[2:end]
-        iterpaths=copy(paths)
-        for path in iterpaths
-            filter!(m->m!=path,paths)
-            newpath=Vector(undef,n)
-            for i in 1:(n-1)
-                newpath[i]=path[i]
-            end
+        newpaths=Set{Set{Int}}()
+        for path in paths
             for element in set
                 if !(element in path)
-                    newpath[end]=element
-                    push!(paths,newpath)
+                    push!(newpaths,union(path,element))
                 end
             end
         end
-        n+=1
+        #println(n)
+        paths=newpaths
+        #println(length(paths))
     end
     return paths
 end
 
-                              
-
-function fully_supported_minors(mat)
+# get strict w types
+function fully_supported_minors(mat::QQMatrix)
     # We tag each column to deal with two that may have identical entries
-    cols=[[i,mat[:,i]] for i in 1:number_of_columns(mat)]
+    cols=[[i,mat[:,i]] for i in 1:number_of_columns(mat)];
     cols_per_sys=[filter(m->!iszero(m[2][i]),cols) for i in 1:number_of_rows(mat)];
-    # OLD: very slow, but doesn't crash julia for larger systems
-    #degenminors=Iterators.product(cols_per_sys...)
-    ## delete those with repeat columns since they will always be det 0
-    ## used to collect first and then filter, this gives OOM
-    #dupeminors=Iterators.filter(m->length(m)==length(unique(m)),degenminors)
-    #minors=collect(dupeminors)
+    cols_per_sys_num=[[c[1] for c in sys] for sys in cols_per_sys]
 
-    # slow, but not as slow as before (?)
-    dupeminors=niceprod_nonrecur(cols_per_sys)
+    minors=niceprod_nonrecur(cols_per_sys_num)
 
-    # select minors unique up to perturbation of columns
-    minors=unique(Set,dupeminors)
-    minors=[[c[2] for c in m] for m in minors]
+    # Convert minors from lists of integers to lists of columns
+    minors=Vector{Vector{QQFieldElem}}[[cols[i][2] for i in minor] for minor in minors]
     return minors
 end
-
-function fully_supported_minors_old(mat)
-    cols=[[i,mat[:,i]] for i in 1:number_of_columns(mat)]
-    cols_per_sys=[filter(m->!iszero(m[2][i]),cols) for i in 1:number_of_rows(mat)];
-    # OLD: very slow, but doesn't crash julia for larger systems
-    degenminors=Iterators.product(cols_per_sys...)
-    ## delete those with repeat columns since they will always be det 0
-    ## used to collect first and then filter, this gives OOM
-    dupeminors=Iterators.filter(m->length(m)==length(unique(m)),degenminors)
-    minors=collect(dupeminors)
-
-    # slow, but not as slow as before (?)
-
-    # select minors unique up to perturbation of columns
-    minors=unique(Set,dupeminors)
-    minors=[[c[2] for c in m] for m in minors]
-    return minors
-end
-
 
 # returns number of zero minors that we consider
-function number_of_zero_minors(mat)
+function number_of_zero_minors(mat::QQMatrix)
     minors=fully_supported_minors(mat)
     niceminors=filter(m->is_det_zero(matrix(QQ,hcat(m...))),minors)
     return length(niceminors)
 end
 
 # returns number of zero minors that we consider
-function number_of_zero_minors_provided_minors(minors)
+function number_of_zero_minors_provided_minors(minors::Vector{Vector{Vector{QQFieldElem}}})
     niceminors=filter(m->is_det_zero(matrix(QQ,hcat(m...))),minors)
     return length(niceminors)
 end
 
-function number_of_fully_supported_minors(mat)
+function number_of_fully_supported_minors(mat::QQMatrix)
     return length(fully_supported_minors(mat))
 end
 
-function data_dump_matrix(sys)
+function data_dump_matrix(sys::OdebaseNode)
     matrix=[]
     perturbations=perturbSystem(sys)
     i=1
@@ -208,9 +178,14 @@ function data_dump_matrix(sys)
         numMinors=binomial(max(numColumns,number_of_rows(mat)),min(numColumns,number_of_rows(mat)))
         row=[per[2],numRelevantMinors,numZeroMinors,numMinors,numColumns]
         push!(matrix,row)
+        i=i+1
     end
     matrix=Matrix(transpose(hcat(matrix...)))
     return matrix
+end
+
+open("odebase/src/rejects.jl","w") do io
+    println(io,rejects)
 end
 
 for sys in systems
@@ -234,8 +209,4 @@ for sys in systems
     open("odebase/src/$name-matrix.jl", "w") do io
         write(io, file)
     end
-end
-
-open("odebase/src/rejects.jl","w") do io
-    println(io,rejects)
 end
